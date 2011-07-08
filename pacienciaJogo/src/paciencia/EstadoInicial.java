f23e6e80ee8b379e9e522e3b2ee2fc106ee705a6
thiago lima <thiagothalison@hotmail.com>
2011-07-08 19:45:06 -0300

    add getters and setters in EstadoInicial
    Change-Id: I50055548b27d1bf45fdd770efcee763fbdb1b45b

--- pacienciaJogo/src/paciencia/EstadoInicial.java
+++ pacienciaJogo/src/paciencia/EstadoInicial.java
@@ -14,5 +14,43 @@ public class EstadoInicial implements Jogada{
     public Collection<ArrayList<Sequencia>> sequencias;
     public Collection<ArrayList<Baralho>> baralhos;
     public int jogadorInicial;
+	public Collection<ArrayList<Pilha>> getPilhas() {
+		return pilhas;
+	}
+	public void setPilhas(Collection<ArrayList<Pilha>> pilhas) {
+		this.pilhas = pilhas;
+	}
+	public Collection<ArrayList<Bagaco>> getBagacos() {
+		return bagacos;
+	}
+	public void setBagacos(Collection<ArrayList<Bagaco>> bagacos) {
+		this.bagacos = bagacos;
+	}
+	public Collection<ArrayList<Restrito>> getRestritos() {
+		return restritos;
+	}
+	public void setRestritos(Collection<ArrayList<Restrito>> restritos) {
+		this.restritos = restritos;
+	}
+	public Collection<ArrayList<Sequencia>> getSequencias() {
+		return sequencias;
+	}
+	public void setSequencias(Collection<ArrayList<Sequencia>> sequencias) {
+		this.sequencias = sequencias;
+	}
+	public Collection<ArrayList<Baralho>> getBaralhos() {
+		return baralhos;
+	}
+	public void setBaralhos(Collection<ArrayList<Baralho>> baralhos) {
+		this.baralhos = baralhos;
+	}
+	public int getJogadorInicial() {
+		return jogadorInicial;
+	}
+	public void setJogadorInicial(int jogadorInicial) {
+		this.jogadorInicial = jogadorInicial;
+	}
+    
+    
 
 }
