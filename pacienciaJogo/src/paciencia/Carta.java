003257d5680f62b13d6f5b9a13fe3db1998409d7
Thalison <thiagothalison@hotmail.com>
2011-07-08 17:06:42 -0300

    commit de carta

--- pacienciaJogo/src/paciencia/Carta.java
+++ pacienciaJogo/src/paciencia/Carta.java
@@ -4,5 +4,17 @@ public class Carta {
 
     private String naipe;
     private Integer numero;
-
+    
+	public String getNaipe() {
+		return naipe;
+	}
+	public void setNaipe(String naipe) {
+		this.naipe = naipe;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}   
 }
